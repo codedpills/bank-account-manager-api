eb0558e91d3cef1a41dfc8066e48bcb58d8408d7
zak <codedpills@gmail.com>
2020-04-11 21:02:24 +0000

    feat: post account, retrieve all / one account(s)

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,44 +1,54 @@
 const Account = require("../models/accountModel");
 
-const accountController = () => {
-  const postAccount = (req, res) => {
-    const account = new Account(req.body);
-    if (!req.body.bankName) {
-      return res.status(400).send("bankName is required!");
+exports.postAccount = (req, res) => {
+  const account = new Account(req.body);
+  if (!req.body.bankName) {
+    return res.status(400).send("bankName is required!");
+  }
+  if (!req.body.bankBranch) {
+    return res.status(400).send("bankBranch is required!");
+  }
+  if (!req.body.accountName) {
+    return res.status(400).send("accountName is required!");
+  }
+  if (!req.body.accountNumber) {
+    return res.status(400).send("accountNumber is required!");
+  }
+  if (!req.body.accountType) {
+    return res.status(400).send("accountType is required!");
+  }
+  account.save((err, account) => {
+    if (err) {
+      return res.status(400).send(`Problem saving account! ${err}`);
     }
-    if (!req.body.bankBranch) {
-      return res.status(400).send("bankBranch is required!");
-    }
-    if (!req.body.accountName) {
-      return res.status(400).send("accountName is required!");
-    }
-    if (!req.body.accountNumber) {
-      return res.status(400).send("accountNumber is required!");
-    }
-    if (!req.body.accountType) {
-      return res.status(400).send("accountType is required!");
-    }
-    account.save((err, account) => {
-      if (err) {
-        return res.status(400).send(`Problem saving account! ${err}`);
-      }
-      res.status(201).json(account);
-    });
-  };
-  const getAccounts = (req, res) => {
-    const query = {};
-    if (req.query.accountType) {
-      query.accountType = req.query.accountType;
-    }
-    Account.find(query, (err, accounts) => {
-      if (err) {
-        return res.status(400).send(`Problem fetching accounts! ${err}`);
-      }
-      return res.status(200).json(accounts);
-    });
-  };
-
-  return { postAccount, getAccounts };
+    return res.status(201).json(account);
+  });
 };
 
-module.exports = accountController;
+exports.getAccounts = (req, res) => {
+  const query = {};
+  if (req.query.accountType) {
+    query.accountType = req.query.accountType;
+  }
+  Account.find(query, (err, accounts) => {
+    if (err) {
+      return res.status(400).send(`Problem fetching accounts! ${err}`);
+    }
+    return res.status(200).json(accounts);
+  });
+};
+
+exports.findAccountById = (req, res, next) => {
+  Account.findById(req.params.accountId, (err, account) => {
+    if (err) {
+      return res.send(err);
+    }
+    if (account) {
+      req.account = account;
+      return next();
+    }
+    return req.sendStatus(404);
+  });
+};
+
+exports.getAccount = (req, res) => res.status(200).json(req.account);
