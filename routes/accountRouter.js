0dd832565b0150e2da9146c56ab6b175c0fa9c1d
zak <codedpills@gmail.com>
2020-04-11 21:03:24 +0000

    chore:  add routes for post, get accounts

--- routes/accountRouter.js
+++ routes/accountRouter.js
@@ -1,16 +1,21 @@
-const express = require('express');
+const express = require("express");
 
-const accountController = require('../controllers/accountController');
-const Account = require('../models/accountModel');
+const Account = require("../models/accountModel");
+const accountController = require("../controllers/accountController");
 
 const routes = () => {
-    const accountRouter = express.Router();
-    accountRouter
-    .route('/account')
+  const accountRouter = express.Router();
+  accountRouter
+    .route("/accounts")
     .post(accountController.postAccount)
     .get(accountController.getAccounts);
+  accountRouter.use("/accounts/:accountId", accountController.findAccountById);
+  accountRouter
+    .route("/accounts/:accountId")
+    .get(accountController.getAccount)
+    .put();
 
-    return accountRouter;
-}
+  return accountRouter;
+};
 
 module.exports = routes;
