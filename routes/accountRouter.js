9cf4eeb6d6c9c55db5c5e2acab710cd60e2e4fd7
zak <codedpills@gmail.com>
2020-04-11 16:15:43 +0000

    chore: set up routes

---
+++ routes/accountRouter.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const accountController = require('../controllers/accountController');
+const Account = require('../models/accountModel');
+
+const routes = () => {
+    const accountRouter = express.Router();
+    accountRouter
+    .route('/account')
+    .post(accountController.postAccount)
+    .get(accountController.getAccount);
+
+    return accountRouter;
+}
+
+module.exports = routes;
